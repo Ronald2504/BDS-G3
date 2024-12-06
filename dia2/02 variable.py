93278730c21a41e16c211b53deb4486d07c4bd87
Bryan Marquina <ronald250497@gmail.com>
2024-12-06 18:14:44 -0500

    dia 1 primeros pasos

---
+++ dia2/02 variable.py
@@ -0,0 +1,7 @@
+# USO DE VARIABLES
+numero1 = input("Ingrese num1: ") 
+numero2 = input("Ingrese num2: ")
+
+suma = int(numero1) + int(numero2)
+
+print(f"La suma de {numero1} + {numero2} es {suma}")
---
+++ dia2/04buclefor.py
@@ -0,0 +1,7 @@
+# BUCLE FOR
+#ENTRADA
+numero = int(input("¿Que numero desea hacer la tabla de multiplicacion: "))
+#PROCESO
+for contador in range(1,13):
+    resultado = contador*numero
+    print(resultado)
