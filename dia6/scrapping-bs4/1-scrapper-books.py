9e807d471db1b9e27dd05e9e25f84d877c313c20
Bryan Marquina <ronald250497@gmail.com>
2025-01-08 20:09:30 -0500

    scrapper

---
+++ dia6/scrapping-bs4/1-scrapper-books.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://books.toscrape.com'
+
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    books = soup.find_all('article', class_='product_pod')
+    
+    for book in books:
+        title=book.find('h3').find('a')['title']
+        price = book.find('p', class_='price_color').get_text()
+        print(title)
+        print(price)
+        print('------------------')
+else:
+    print(f'Error {response.status_code}')
